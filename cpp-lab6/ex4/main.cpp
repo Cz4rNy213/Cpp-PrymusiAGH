de1825e30d5f67e81b02c93d40ef6a8b373f3a33
Cz4rNy213 <mikolaj.kolek2@gmail.com>
2023-12-15 15:27:45 +0100

    lab6 finished

--- cpp-lab6/ex4/main.cpp
+++ cpp-lab6/ex4/main.cpp
@@ -9,7 +9,7 @@
 
 class Preprocessor {
     public:
-        Preprocessor(): define_token_map(), result(), text_strings(), text_stream(){}
+        Preprocessor(): define_token_map(), result(), text_strings(), text_stream(){} //initializing with default empty values
 
         void readFile(const char*);
         void processTokens();
@@ -32,64 +32,86 @@ void Preprocessor::readFile(const char* file_name){
         throw std::runtime_error("Can not open file in readFile function");
     }
     text_stream << file.rdbuf();
-    text_strings = text_stream.str();
+    text_strings = text_stream.str(); //reading file and saving it as string
 }
 
 void Preprocessor::processTokens(){
-    std::istringstream text(text_strings);
-
+    std::istringstream text(text_strings); //text to iterate through
+    bool include_line = true; //include line or not, necessary for #JeśliZdefiniowane #JeśliNie #Zakończ
+    bool include_else = false; //as above
     std::string line;
-    while (getline(text, line)){
+    while (getline(text, line)){ //iterating through each line
             std::istringstream line_stream(line);
             std::string token;
-        while(line_stream >> token){
+        while(line_stream >> token){ //iterating through each string in line
             if (token == "#inkluduj"){
             std::string header_file;
             line_stream >> header_file;
-            if(!handleInclude(token, header_file)){
+            if(!handleInclude(token, header_file)){ //check if header file for #inkluduj is correct
                 std::cerr << "Error in handling #inkluduj. Exiting...\n";
                 std::exit(EXIT_FAILURE);
             }
-            result << token + ' ';
-            result << header_file + ' ';
+            result << token + ' '; 
+            result << header_file + ' '; 
         }
         else if (token == "#definiuj"){
             std::string def_name;
             std::string value_string;
             int value;
-            line_stream >> def_name;
-            line_stream >> value_string;
-            if(!handleDefine(def_name, value_string, value)){
+            line_stream >> def_name; //get name for #definiuj
+            line_stream >> value_string; //get value
+            if(!handleDefine(def_name, value_string, value)){ //check if name and value for #definiuj is correct
                 std::cerr << "Error in handling #definiuj. Exiting...\n";
                 std::exit(EXIT_FAILURE);
             }
-            define_token_map[def_name] = value;
+            define_token_map[def_name] = value; //adding name and value to map so i can then change definied names with their values
+        }
+        else if (token == "#JeśliZdefiniowane"){ //#ifdef
+            std::string def_name;
+            line_stream >> def_name;
+            auto it = define_token_map.find(def_name);
+            if (!(it != define_token_map.end())){ //check if name was defined
+                include_line = false;
+                include_else = true;
+            }
+        }
+        else if (token == "#JeśliNie"){ //#else
+            include_line = include_else;
+            include_else = false;
+        }
+        else if (token == "#Zakończ"){ //#endif
+            include_line = true;
+            include_else = false;
         }
         else{
-            auto it = define_token_map.find(token);
-            if (it != define_token_map.end()){
-                result << it->second << ' ';
-            }
-            else{
-                result << token + ' ';
+            if (include_line){
+                auto it = define_token_map.find(token); 
+                if (it != define_token_map.end()){ //if token in map then change it with corresponding value
+                    result << it->second << ' ';
+                }
+                else{
+                    result << token + ' ';
+                }
             }
         }
         } 
-        result << "\n";
+        if (include_line){
+            result << "\n"; //endline after reading each line
+        }
     }
     writeResultToFile(result);
 }
 
 bool Preprocessor::handleInclude(std::string& token, std::string& header_file){
-    if (header_file[0] != '<' && header_file[header_file.size()] - 1 != '>'){
+    if (header_file[0] != '<' && header_file[header_file.size()] - 1 != '>'){ //check if #inkluduj header file contains "< ... >"
         throw std::runtime_error("Incorrect header file: " + header_file);
         return false;
     }
-    else if (header_file.size() <= 2){
+    else if (header_file.size() <= 2){ //if header file contains only "<>" then return error
         throw std::runtime_error("Incorrect header file: " + header_file);
         return false;
     }
-    token = "#include";
+    token = "#include"; //change token name to #include so result file can include libraries correctly
     return true;
 }
 
@@ -102,8 +124,7 @@ bool Preprocessor::handleDefine(const std::string& def_name, std::string& value_
     return true;
 }
 
-
-int Preprocessor::convertStringToInt(std::string& value_string){
+int Preprocessor::convertStringToInt(std::string& value_string){ //converting string value from #zdefiniuj to int so i can add it to map
     try {
         return std::stoi(value_string);
     }
@@ -119,21 +140,21 @@ int Preprocessor::convertStringToInt(std::string& value_string){
 
 bool Preprocessor::validDefName(std::string def_name){
     for (int i = 0; i < def_name.size(); i++){
-        if (!((def_name[i] >= 'A' && def_name[i] <= 'Z') || def_name[i] == '_')){
+        if (!((def_name[i] >= 'A' && def_name[i] <= 'Z') || def_name[i] == '_')){ // check if #zdefiniuj name contains only upper case letters or "_"
             return false;
         }
     }
     return true;
 }
 
-void Preprocessor::writeResultToFile(const std::ostringstream& result){
-    std::ofstream outfile("result.cpp");
+void Preprocessor::writeResultToFile(const std::ostringstream& result){ 
+    std::ofstream outfile("result.cpp"); //you can type any name you want, just remember to add .cpp at the end, programm will create file for you
     outfile << result.str();
 }
 
 int main(){
     Preprocessor preprocessor;
-    preprocessor.readFile("input.cpp");
+    preprocessor.readFile("input.cpp"); //type here input file you want to compile
     preprocessor.processTokens();
     return 0;
 }
--- cpp-lab6/ex4/result.cpp
+++ cpp-lab6/ex4/result.cpp
@@ -1,10 +1,24 @@
 #include <iostream> 
 
 
+
+
 int main() { 
 int radius = 5; 
 double area = 3 * radius * radius; 
 std::cout << "Area of the circle: " << area << std::endl; 
 std::cout << 100 ; 
+
+std::cout<<"PI defined correctly"<<std::endl; 
+
+
+
+std::cout<<"MAXX not defined correctly"<<std::endl; 
+
+
+
+std::cout<<"MAX_VALUE defined correctly"<<std::endl; 
+
+
 return 0; 
 } 
